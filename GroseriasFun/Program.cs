ebd1b817f06b48f704978c6ca133eb931c1c7f34
immanuel448 <emmanuelmendoza448@gmail.com>
2025-09-04 16:38:45 -0600

    por menores inciales

--- GroseriasFun/Program.cs
+++ GroseriasFun/Program.cs
@@ -1,19 +1,26 @@
-﻿using System.Text.Json;
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using GroseriasFun;
 
-string filePath = "groserias.json";
-string json = File.ReadAllText(filePath);
-
-var groserias = JsonSerializer.Deserialize<List<Groseria>>(json);
-
-foreach (var g in groserias)
+class Program
 {
-    Console.WriteLine($"ES: {g.Es} | EN: {g.En} | FR: {g.Fr}");
+    static void Main()
+    {
+        string filePath = "groserias.json";
+        string json = File.ReadAllText(filePath);
+
+        var groserias = JsonSerializer.Deserialize<List<Groseria>>(json);
+
+        foreach (var g in groserias)
+        {
+            Console.WriteLine($"ES: {g.Es} | EN: {g.En} | FR: {g.Fr}");
+        }
+
+        Console.WriteLine("\nFin del listado. Presiona cualquier tecla para salir.");
+        Console.ReadKey();
+    }
 }
 
-public class Groseria
-{
-    public int Id { get; set; }
-    public string Es { get; set; }
-    public string En { get; set; }
-    public string Fr { get; set; }
-}
