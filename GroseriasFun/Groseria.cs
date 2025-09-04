94810ec05f2c8bc39529de2244d37671aec31a4c
immanuel448 <emmanuelmendoza448@gmail.com>
2025-09-04 16:19:54 -0600

    Commit inicial del proyecto GroseriasFun

---
+++ GroseriasFun/Groseria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroseriasFun
+{
+    public class Groseria
+    {
+        public int Id { get; set; }
+        public string Es { get; set; }
+        public string En { get; set; }
+        public string Fr { get; set; }
+        // Futuro: Descripcion, Imagen, Audio
+
+    }
+}
---
+++ GroseriasFun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+string filePath = "groserias.json";
+string json = File.ReadAllText(filePath);
+
+var groserias = JsonSerializer.Deserialize<List<Groseria>>(json);
+
+foreach (var g in groserias)
+{
+    Console.WriteLine($"ES: {g.Es} | EN: {g.En} | FR: {g.Fr}");
+}
+
+public class Groseria
+{
+    public int Id { get; set; }
+    public string Es { get; set; }
+    public string En { get; set; }
+    public string Fr { get; set; }
+}
